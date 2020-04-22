c593bb22956529a49087efc065f31889b576070b
mprzybyla123 <mprzybyla123@gmail.com>
2020-04-21 20:14:19 -0500

    Add read_df

--- example/read_df.py
+++ example/read_df.py
@@ -9,7 +9,7 @@ import pandas as pd
 class testClass(IntroductionClass):
 
     def read_data(self):
-        df = pd.read_csv('/Users/MatthewPrzybyla/Downloads/DummyData_Page 1_Time series.csv')
+        df = pd.read_csv('/Users/data.csv')
         df['Date'] = pd.to_datetime(df['Year Month'])
         self.data = df
         return self.data.head()
